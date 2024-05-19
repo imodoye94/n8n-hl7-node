68c2456f63c98b4d6c43278631df3fb74dd570fc
IMODOYE AYOKUNBI ABIORO <44514227+imodoye94@users.noreply.github.com>
2024-05-19 08:07:01 +0100

    Update HL7Node.node.ts
    change icon format back to svg

--- nodes/HL7Node/HL7Node.node.ts
+++ nodes/HL7Node/HL7Node.node.ts
@@ -16,7 +16,7 @@ export class HL7Node implements INodeType {
   description: INodeTypeDescription = {
     displayName: 'HL7 Node',
     name: 'hl7Node',
-    icon: 'file:hl7Node.png',
+    icon: 'file:hl7Node.svg',
     group: ['transform'],
     version: 1,
     description: 'Parse JSON to HL7 and generate HL7 messages from JSON',
